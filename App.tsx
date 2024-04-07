203335865d2fc44a5c0826ef11e0212fe5dfc5c9
haipv01 <haipv@bsscommerce.com>
2024-04-07 23:16:05 +0700

    share transition

--- App.tsx
+++ App.tsx
@@ -1,118 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import * as React from 'react';
+import { View, Button, Dimensions } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Animated, { FadeInLeft } from 'react-native-reanimated';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const Stack = createNativeStackNavigator();
+const {width} = Dimensions.get('window')
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function Screen1({ navigation }) {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {/* <Animated.View
+        style={{ width: 150, height: 150, backgroundColor: 'green' }}
+        sharedTransitionTag="sharedTag"
+      /> */}
+       <Animated.Text  entering={FadeInLeft.duration(400).delay(500)} style={{marginVertical: 50, fontSize: 19, paddingHorizontal: 100}}>
+      The Serendipitous Symphony: Exploring the Unforeseen Harmonies of Life
+      </Animated.Text>
+       <Animated.Image source={{uri: 'https://t4.ftcdn.net/jpg/03/08/25/59/360_F_308255920_Hk8uUfEH4t4JpoiidNyxL02EKie8dONk.jpg'}} 
+      style={{ width: width, height: 250}} resizeMode={'contain'} sharedTransitionTag="sharedTag" />
+      <Button title="Screen2" onPress={() => navigation.navigate('Screen2')} />
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+function Screen2({ navigation }) {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={{ flex: 1 }}>
+      {/* <Animated.View
+        style={{ width: width, height: 250, backgroundColor: 'red' }}
+        sharedTransitionTag="sharedTag"
+      /> */}
+       <Animated.Image source={{uri: 'https://t4.ftcdn.net/jpg/03/08/25/59/360_F_308255920_Hk8uUfEH4t4JpoiidNyxL02EKie8dONk.jpg'}} 
+      style={{ width: width, height: 250}} resizeMode={'contain'} sharedTransitionTag="sharedTag" />
+      <Animated.Text entering={FadeInLeft.duration(400).delay(500)} style={{marginVertical: 50, fontSize: 19, paddingHorizontal: 100}}>
+      The Serendipitous Symphony: Exploring the Unforeseen Harmonies of Life
+      </Animated.Text>
+      <Button title="Screen1" onPress={() => navigation.navigate('Screen1')} />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default function SharedElementExample() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: true }}>
+        <Stack.Screen name="Screen1" component={Screen1} />
+        <Stack.Screen name="Screen2" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
